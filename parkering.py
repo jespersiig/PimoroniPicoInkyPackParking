0baf6aeb92eac97410669ea6d6e8f95b678c4662
Jesper E. Siig <32156581+jespersiig@users.noreply.github.com>
2023-03-07 00:17:37 +0100

    Update parkering.py
    Explanation of Danish texts

--- parkering.py
+++ parkering.py
@@ -8,7 +8,7 @@ def clear():
     inky.set_pen(15)
     inky.clear()
     inky.update()
-    
+
 def display_address():
     clear()
     inky.set_pen(0)
@@ -16,7 +16,7 @@ def display_address():
     inky.text(address, 10, 50, 240, 3)
     inky.text(lead_text, 10, 110, 240, 2)
     inky.update()
-    
+
 # Buttons
 button_a = machine.Pin(12, machine.Pin.IN, pull=machine.Pin.PULL_UP)
 button_b = machine.Pin(13, machine.Pin.IN, pull=machine.Pin.PULL_UP)
@@ -29,15 +29,15 @@ inky.set_update_speed(3)
 inky.set_font("bitmap8")
 
 streets = ["Jul. Bloms Gade","Husumgade","Jagtvej"]
-settings = ["gade","tiere","enere","slut"]
+settings = ["gade","tiere","enere","slut"] # street, tens, ones, finish
 set_address = False
 index = 0
 street_index = 0
 tens = 0
 ones = 4
-top_text = "Bilen står ud for:"
+top_text = "Bilen står ud for:" # The car is in front of
 address = streets[street_index]+" "+str(tens+ones)
-lead_text = "Tryk på B for at ændre"
+lead_text = "Tryk på B for at ændre" # Press B to change
 display_address()
 
 # Button handling function
@@ -45,11 +45,11 @@ def button(pin):
     global last, set_address, index, streets, street_index, tens, ones, address, lead_text, top_text
 
     address = streets[street_index]+" "+str(tens+ones)
-    print("Før: "+address)
+    print("Før: "+address) # Debug text "Before: + address"
 
     adjust = 0
     changed = False
-    
+
     time.sleep(0.01)
     if pin.value():
         return
@@ -57,8 +57,8 @@ def button(pin):
     if pin == button_b and not set_address:
         index = 0
         set_address = True
-        top_text = "Vælg gade"
-        lead_text = "A: op, C: ned, B: næste punkt"
+        top_text = "Vælg gade" # Chose street
+        lead_text = "A: op, C: ned, B: næste punkt" # A: up, C: down, B: Next inout
         display_address()
         return
 
@@ -66,7 +66,7 @@ def button(pin):
         if pin == button_b:
             index += 1
             index %= len(settings)
-            top_text = "Vælg "+ settings[index]
+            top_text = "Vælg "+ settings[index] # Chose settings[index]
             changed = True
 
         if pin == button_a:
@@ -90,14 +90,14 @@ def button(pin):
         if settings[index] == "slut":
             set_address = False
             changed = True
-            top_text = "Bilen står ud for:"
+            top_text = "Bilen står ud for:" # The car is in front of
             address = streets[street_index]+" "+str(tens+ones)
-            lead_text = "Tryk på B for at ændre"
-            print("Finale: "+address)
+            lead_text = "Tryk på B for at ændre" # Press B to change
+            print("Finale: "+address) # Debug text "Final: + address"
 #            return
 
     address = streets[street_index]+" "+str(tens+ones)
-    print("Efter: "+address)
+    print("Efter: "+address) # Debug text "After: + address"
     display_address()
 
 button_a.irq(trigger=machine.Pin.IRQ_FALLING, handler=button)
